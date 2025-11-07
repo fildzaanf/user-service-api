47664e9654dbdfa839ef89d5cc0b4155f066aa7c
fildzaanf <hanisahfildza@gmail.com>
2025-11-07 13:16:31 +0700

    update interceptor with token

--- pkg/middleware/interceptor.go
+++ pkg/middleware/interceptor.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"errors"
-	"fmt"
 	"os"
 	"strings"
 
@@ -19,15 +18,25 @@ type claimKey string
 const (
 	ClaimUserID claimKey = "user_id"
 	ClaimRole   claimKey = "role"
+	ClaimToken  claimKey = "token"
 )
 
 func JWTUnaryInterceptor() grpc.UnaryServerInterceptor {
+	publicMethodsEnv := os.Getenv("PUBLIC_METHODS")
+	publicMethods := strings.Split(publicMethodsEnv, ",")
+
 	return func(
 		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
 	) (interface{}, error) {
+
+		for _, method := range publicMethods {
+			if info.FullMethod == strings.TrimSpace(method) {
+				return handler(ctx, req)
+			}
+		}
 
 		md, ok := metadata.FromIncomingContext(ctx)
 		if !ok {
@@ -39,14 +48,15 @@ func JWTUnaryInterceptor() grpc.UnaryServerInterceptor {
 			return nil, status.Error(codes.Unauthenticated, "missing authorization token")
 		}
 
-		tokenString := strings.TrimPrefix(authHeader[0], "Bearer ")
-		fmt.Println("Token string:", tokenString)
+		rawToken := authHeader[0]
+
+		tokenString := strings.TrimSpace(strings.TrimPrefix(rawToken, "Bearer "))
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			return []byte(os.Getenv("JWT_SECRET")), nil
 		})
 		if err != nil || !token.Valid {
-			return nil, status.Error(codes.Unauthenticated, "invalid authorization token")
+			return nil, status.Error(codes.Unauthenticated, "invalid token")
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
@@ -56,25 +66,34 @@ func JWTUnaryInterceptor() grpc.UnaryServerInterceptor {
 
 		id, _ := claims["id"].(string)
 		role, _ := claims["role"].(string)
+
 		if id == "" || role == "" {
 			return nil, status.Error(codes.Unauthenticated, "invalid token claims")
 		}
+
 		ctx = context.WithValue(ctx, ClaimUserID, id)
 		ctx = context.WithValue(ctx, ClaimRole, role)
+		ctx = context.WithValue(ctx, ClaimToken, tokenString)
 
 		return handler(ctx, req)
 	}
 }
 
-
-func ExtractTokenFromContext(ctx context.Context) (string, string, error) {
+func ExtractTokenFromContext(ctx context.Context) (string, string, string, error) {
 	id, ok := ctx.Value(ClaimUserID).(string)
 	if !ok {
-		return "", "", errors.New("user id not found in context")
+		return "", "", "", errors.New("user id not found in context")
 	}
+
 	role, ok := ctx.Value(ClaimRole).(string)
 	if !ok {
-		return "", "", errors.New("role not found in context")
+		return "", "", "", errors.New("role not found in context")
 	}
-	return id, role, nil
+
+	token, ok := ctx.Value(ClaimToken).(string)
+	if !ok || token == "" {
+		return "", "", "", errors.New("raw token not found in context")
+	}
+
+	return id, role, token, nil
 }
